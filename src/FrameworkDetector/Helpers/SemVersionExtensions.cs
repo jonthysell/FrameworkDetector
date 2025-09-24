240e8fa8dc8dc07626b8f7e9c4d0bc2f52747ad4
Jon Thysell <jthysell@microsoft.com>
2025-09-24 16:14:38 -0700

    Made our SemVersion parsing more robust for the strange version strings in the wild.

--- src/FrameworkDetector/Helpers/SemVersionExtensions.cs
+++ src/FrameworkDetector/Helpers/SemVersionExtensions.cs
@@ -25,10 +25,31 @@ internal static class SemVersionExtensions
                 {
                     if (fex.Message.StartsWith("Fourth version number"))
                     {
+                        // Remove the fourth version number part and try again
                         version = string.Join('.', version.Split('.').Take(3));
-                        return SemVersion.TryParse(version, SemVersionStyles.Any, out semver);
+                        return SemVersion.TryLooseParse(version, out semver);
                     }
+                    else if (fex.Message.Contains("contains invalid character ' '"))
+                    {
+                        // Some version strings have extra content after a space, take just the version and try again
+                        version = version.Split(' ')[0];
+                        return SemVersion.TryLooseParse(version, out semver);
+                    }
+                    else if (fex.Message.Contains("contains invalid character ','"))
+                    {
+                        // Some version strings use ',' or ', ' instead of '.' as a delimiter, reformat and try again
+                        version = version.Replace(", ", ".").Replace(',', '.');
+                        return SemVersion.TryLooseParse(version, out semver);
+                    }
+#if DEBUG
+                    else
+                    {
+                        // Log other unexpected formatting errors
+                        Console.Error.WriteLine(fex.Message);
+                    }
+#endif
                 }
+                catch { }
             }
 
             semver = default;
